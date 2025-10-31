c97d6477579bbc89bfd1f4743a31b72db0d7115d
Onur Bingol <obingol@anaconda.com>
2025-10-31 02:30:05 -0700

    make sure to remove auto-generated c/cpp files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,7 @@
 set DIR_NAME=%PKG_NAME:-=_%
 cd %DIR_NAME%
 %PYTHON% -m pip install . --no-deps --no-build-isolation -vv
+
+:: Clean up Cython temporary files from site-packages directory
+del /s /q %SP_DIR%\*.c
+del /s /q %SP_DIR%\*.cpp
